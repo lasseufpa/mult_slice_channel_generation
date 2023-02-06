1b22bdbca7f97396f91aa0740478475be6a382a9
Cleverson Nahum <cleversonahum@gmail.com>
2023-02-06 15:42:53 -0300

    Starting to evaluate MTs tracking

---
+++ src/ues_track.m
@@ -0,0 +1,16 @@
+close all
+clear all
+
+rng(10)													% Constant seed
+
+s = qd_simulation_parameters;                           % Set up simulation parameters
+s.show_progress_bars = 1;                               % Show progress bars
+s.center_frequency = 2.53e9;                            % Set center frequency
+s.samples_per_meter = 1;                                % 1 sample per meter
+s.use_absolute_delays = 1;                              % Include delay of the LOS path
+
+l = qd_layout(s);                                       % Create new QuaDRiGa layout
+l.no_rx = 2;                                          % Set number of MTs
+l.randomize_rx_positions( 1000 , 1.5 , 1.5 , 500 );      % 200 m radius, 1.5 m Rx height
+l.visualize([], [], 0)
+print("Test")
