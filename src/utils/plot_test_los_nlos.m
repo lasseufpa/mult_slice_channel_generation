92f4ac3b7bfb4ca10b3e9cbbbffc966cb7059dbf
Cleverson Nahum <cleversonahum@gmail.com>
2023-02-24 19:52:55 -0300

    plotting power received between one TX and RX antenna, but without LOS and NLOS indication)

--- src/utils/plot_test_los_nlos.m
+++ src/utils/plot_test_los_nlos.m
@@ -3,17 +3,20 @@
 tx_number = 1;
 rx_number = 1;
 scenario = "3GPP-38.901-UMa";
-los_nlos = "NLOS";
-number_segments = 1000;
+number_segments = 100;
 path_channels = "../../results/channel/";
 
-power_time = zeros(1, number_segments);
+power_time = zeros(number_segments);
+los_or_nlos = cell(number_segments);
 for idx_segment=1:number_segments
 	channel_file = dir([path_channels, scenario,"-*_Tx", num2str(tx_number, '%04.f'), "_Rx", num2str(rx_number, '%04.f'), "_seg", num2str(idx_segment, '%04.f'), ".mat"]);
+	los_or_nlos{idx_segment} = erase(lower(channel_file.name(1, size(scenario)(2)+2:size(scenario)(2)+5)), "_");
 	channel_step = qd_channel.mat_load([path_channels, channel_file.name]);
-	power_time(idx_segment) = sum(abs(channel_step.coeff(:,:,:)).^2, 3);
-	disp("test");
+	power_time(idx_segment) = 10*log10(sum(sum(abs(channel_step.coeff(:,:,:)).^2)));
+	fprintf("Segment %d \n", idx_segment)
 end
-fig_plot = 0;
 
-% end
+set(0,'DefaultFigurePaperSize',[14.5 4.7])              % Change Plot Size 
+plot(spower_time);
+
+disp("Test")
