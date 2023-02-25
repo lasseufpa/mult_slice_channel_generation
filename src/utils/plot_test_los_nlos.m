95e6944a78078258be3ee532ec4883b3f1f7c58c
Cleverson Nahum <cleversonahum@gmail.com>
2023-02-25 10:36:59 -0300

    Plotting received power and LOS area

--- src/utils/plot_test_los_nlos.m
+++ src/utils/plot_test_los_nlos.m
@@ -1,13 +1,11 @@
-% function fig_plot =  plot_test_los_nlos()
-
 tx_number = 1;
 rx_number = 1;
 scenario = "3GPP-38.901-UMa";
-number_segments = 100;
+number_segments = 1000;
 path_channels = "../../results/channel/";
 
-power_time = zeros(number_segments);
-los_or_nlos = cell(number_segments);
+power_time = zeros(1, number_segments);
+los_or_nlos = cell(1, number_segments);
 for idx_segment=1:number_segments
 	channel_file = dir([path_channels, scenario,"-*_Tx", num2str(tx_number, '%04.f'), "_Rx", num2str(rx_number, '%04.f'), "_seg", num2str(idx_segment, '%04.f'), ".mat"]);
 	los_or_nlos{idx_segment} = erase(lower(channel_file.name(1, size(scenario)(2)+2:size(scenario)(2)+5)), "_");
@@ -17,6 +15,24 @@ for idx_segment=1:number_segments
 end
 
 set(0,'DefaultFigurePaperSize',[14.5 4.7])              % Change Plot Size 
-plot(spower_time);
 
+# Plot LOS area
+idx_los = find(ismember(los_or_nlos, "los"));
+diff_idx = diff([0,diff(idx_los)==1,0]);
+first_idx = idx_los(diff_idx>0);
+last_idx = idx_los(diff_idx<0);
+
+for area_idx=1:size(first_idx)(2)
+	area([first_idx(area_idx), last_idx(area_idx)], repmat(-160, 1, 2),'FaceColor',"cyan",'LineStyle','none');
+	hold on
+end
+
+# Plot received power
+plot(power_time);
+hold off
+grid on
+ylim([-140, -80])
+xlabel("Simulation steps (n)");
+ylabel("RX power (dBm)");
+saveas(gcf, "../../results/layout/los_nlos_power.png");
 disp("Test")
