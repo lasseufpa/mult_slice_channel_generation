252b1326f512263009af5f033f651946a2db77d9
Cleverson Nahum <cleversonahum@gmail.com>
2023-02-28 15:43:44 -0300

    Plotting received power for a specific TX and RX combination

--- src/utils/plot_test_los_nlos.m
+++ src/utils/plot_test_los_nlos.m
@@ -3,22 +3,12 @@ rx_number = 1;
 scenario = '3GPP-38.901-UMa';
 number_snapshots = 1000;
 path_channels = 'results/channel/';
+path_result = 'results/layout/';
 
 channel_step = qd_channel.mat_load([path_channels, 'Tx', num2str(tx_number, '%04.f'), '_Rx', num2str(rx_number, '%04.f'), '.mat']);
 power_time = 10*log10(squeeze(sum(sum(sum(abs(channel_step.coeff(:,:,:,:)).^2, 1), 2), 3))');
 
 set(0,'DefaultFigurePaperSize',[14.5 4.7])              % Change Plot Size 
-
-% % Plot LOS area
-% idx_los = find(ismember(los_or_nlos, "los"));
-% diff_idx = diff([0,diff(idx_los)==1,0]);
-% first_idx = idx_los(diff_idx>0);
-% last_idx = idx_los(diff_idx<0);
-
-% for area_idx=1:size(first_idx, 2)
-% 	area([first_idx(area_idx), last_idx(area_idx)], repmat(-160, 1, 2),'FaceColor',"cyan",'LineStyle','none');
-% 	hold on
-% end
 
 % Plot received power
 plot(power_time);
@@ -27,5 +17,4 @@ grid on
 ylim([-140, -80])
 xlabel("Simulation steps (n)");
 ylabel("RX power (dBm)");
-saveas(gcf, "../../results/layout/los_nlos_power.png");
-disp("Test")
+saveas(gcf, [path_result, 'received_power_tx', num2str(tx_number), '_rx', num2str(rx_number), '.png']);
