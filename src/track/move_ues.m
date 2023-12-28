62fa8bbd01f73113691e89b592ea64f01479562c
Cleverson Nahum <cleversonahum@gmail.com>
2023-12-28 12:43:29 -0500

    Avoiding loops taking too long

--- src/track/move_ues.m
+++ src/track/move_ues.m
@@ -6,7 +6,7 @@ function [reshaped_positions, directions] = move_ues (positions, initial_positio
 	for ue_idx=1:n_ues
 		ue_norm = calc_ue_norm(x_positions(ue_idx), y_positions(ue_idx), initial_positions(1:2,ue_idx));
 		while or(ue_norm > max_bs_radius, ue_norm < min_dist_ue_bs)
-			directions(ue_idx) = rand()*2*pi;
+			directions(ue_idx) = directions(ue_idx) + pi/4;
 			[x_positions, y_positions] = polar_to_cartesian(positions, n_step, n_ues, speed_per_ue, directions);
 			ue_norm = calc_ue_norm(x_positions(ue_idx), y_positions(ue_idx), initial_positions(1:2,ue_idx));
 		end
